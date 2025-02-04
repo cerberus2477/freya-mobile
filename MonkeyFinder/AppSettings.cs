2ffa2fced448c1bb390b99edd37ddee9de1143f5
3041TAN-12 <3041TAN-12@tanuzem.boronkay.local>
2025-02-04 14:53:35 +0100

    readme modositasa es remelem nem rontottam el az egesz szerkezetet

---
+++ MonkeyFinder/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreyaDev
+{
+    public static class AppSettings
+    {
+        //később ezt át kell írni dev tunnelinggel hogy elérhető legyen telefonról is
+        public static string ApiBaseUrl { get; } = "http://127.0.0.1:8069/api/";
+    }
+}
